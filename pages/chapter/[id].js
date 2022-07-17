ab75600f360981edffa378ef0396ad49cf85d4a2
pedro valdez <pedro@valdez>
2022-07-17 14:32:23 -0500

    Pages are now contentful

--- pages/chapter/[id].js
+++ pages/chapter/[id].js
@@ -4,7 +4,25 @@ import { getChapterImages } from '../../lib/fetching/mangadex'
 export default function Chapter({ images }) {
   console.log(images)
 
-  return <div></div>
+  return (
+    <main>
+      <div className='h-screen carousel carousel-vertical'>
+        {
+          images.images.map(image => (
+            <div
+              className='carousel-item h-full'
+              key={image}>
+              <img
+                src={`${images.baseUrl}/data/${images.hash}/${image}`}
+                alt=""
+                className="carousel-item mx-auto"
+              />
+            </div>
+          ))
+        }
+      </div>
+    </main>
+  )
 }
 
 export async function getStaticProps({ params: { id } }) {
--- pages/manga/[id].js
+++ pages/manga/[id].js
@@ -1,15 +1,40 @@
+import Hero from '../../components/MangaHero'
+import Feed from '../../components/Feed'
 import { getManga, getMangaFeed } from '../../lib/fetching/mangadex'
 
 
 export default function Manga({ manga, feed }) {
-  console.log(manga, feed)
+  return (
+    <div>
+      <Hero {...manga}/>
 
-  return <div></div>
+      <Feed feed={feed}/>
+    </div>
+  )
 }
 
 export async function getStaticProps({ params: { id } }) {
-  const manga = await getManga(id, {})
-  const feed = await getMangaFeed(id, {})
+  const manga = await getManga(id, {
+    params: {
+      includes: ['cover_art'],
+    },
+    includes: {
+      coverArt: true,
+    },
+  })
+  const feed = await getMangaFeed(id, {
+    params: {
+      includes: ['scanlation_group', 'user'],
+      'order[volume]': 'desc',
+      'order[chapter]': 'desc',
+      offset: 0,
+      contentRating: ['safe', 'suggestive', 'erotica', 'pornographic'],
+    },
+    includes: {
+      scanlationGroup: true,
+      user: true,
+    },
+  })
 
   return {
     props: {
